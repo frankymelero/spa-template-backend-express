32d3a04b0ebf1bb525a48192cc0215f1133594d9
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2023-09-21 19:02:59 +0200

    Vercel deploy test

--- src/services/appointment.service.js
+++ src/services/appointment.service.js
@@ -1,4 +1,5 @@
 const { models } = require('../libs/sequelize');
+const nodemailer = require('nodemailer');
 
 class AppointmentService {
 
@@ -29,6 +30,7 @@ class AppointmentService {
         const res = await models.Appointment.create(data);
         return res;
     }
+
 
     async update(id, data){
         const model = await this.findOne(id);
@@ -66,15 +68,43 @@ class AppointmentService {
           });
       
           if (model) {
+            // Obtén los detalles de la cita desde la base de datos
+            const { date, hour, name, email, token } = model.dataValues;
+      
             // Actualiza el campo validated a true
             await model.update({ validated: true });
+      
+            // Construye el enlace con el token
+            const link = `https://loqueseaq.com/appointment/${token}`;
+      
+            // Personaliza el mensaje del correo electrónico con los datos de la cita y el enlace
+            const message = `La cita para el ${date} a las ${hour} con el nombre ${name} (${email}) y token ${token} se ha validado exitosamente. Puedes acceder a tu cita <a href="${link}">aquí</a>.`;
+      
+            // Configura el transporte de Nodemailer
+            const transporter = nodemailer.createTransport({
+              service: 'Gmail',
+              auth: {
+                user: 'franmelerogallardo@gmail.com',
+                pass: 'Dioesdiosp6hp1w'
+              }
+            });
+      
+            // Define las opciones del correo electrónico
+            const mailOptions = {
+              from: 'franmelerogallardo@gmail.com',
+              to: email, // Utiliza la dirección de correo electrónico obtenida de la base de datos
+              subject: 'Validación de Cita',
+              html: message, // Usa el mensaje personalizado como HTML
+            };
+      
+            // Envía el correo electrónico
+            await transporter.sendMail(mailOptions);
       
             return { success: true, message: "Cita validada exitosamente." };
           } else {
             return { success: false, message: "No se encontró una cita con el token proporcionado." };
           }
         } catch (error) {
-          // Maneja cualquier error que pueda ocurrir durante la consulta a la base de datos
           console.error('Error al validar cita por token:', error);
           throw error;
         }
