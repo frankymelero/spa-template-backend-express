47b7e2778a41dcbf64aa9a873ba287fc1450e92b
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2023-09-22 16:08:21 +0200

    chequ

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -2,17 +2,17 @@ const { Sequelize } = require('sequelize');
 
 const { config } = require('../config/config');
 const setupModels = require('./../db/models');
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-
-config.dbName,
-config.dbUser,
-config.dbPassword,
-{
-    host: config.dbHost,
-    dialect: 'postgresql'
-}
-);
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialect: 'postgres',
+    ssl: true, // Habilita SSL si es necesario (puede depender de tu configuración de base de datos)
+    dialectOptions: {
+      ssl: {
+        require: true, // Opcional, dependiendo de la configuración de tu base de datos
+      },
+    },
+  });
 
 sequelize.sync();
 setupModels(sequelize);
