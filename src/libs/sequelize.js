a7876dd263e9b477174d574efb79bb127f0701d0
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2023-09-22 16:21:32 +0200

    vercel new

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -4,14 +4,12 @@ const { config } = require('../config/config');
 const setupModels = require('./../db/models');
 require('dotenv').config();
 
+import * as pg from 'pg';
+import { Sequelize } from 'sequelize';
+
 const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialectModule: pg,
     dialect: 'postgres',
-    ssl: true, // Habilita SSL si es necesario (puede depender de tu configuración de base de datos)
-    dialectOptions: {
-      ssl: {
-        require: true, // Opcional, dependiendo de la configuración de tu base de datos
-      },
-    },
   });
 
 sequelize.sync();
